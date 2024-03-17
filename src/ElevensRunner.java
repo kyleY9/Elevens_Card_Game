a9992c3c5963ba0d320aeee7152b2c4505cda42c
Kyle Yang <KyleY9@nycstudents.net>
2024-03-17 17:05:34 -0400

    I have finished my Elevens game. Ready for submission!

--- src/ElevensRunner.java
+++ src/ElevensRunner.java
@@ -7,26 +7,43 @@ public class ElevensRunner {
         System.out.println("Welcome to Elevens, try your best as this game is quite hard!");
         System.out.println("Cards left to be dealt: " + board.deckSize());
 
-        System.out.println(board);
         while (!(board.gameIsWon())) {
-            System.out.println("Current cards on the field:\n" + board);
-            int sizeOfArr = 0;
-            String[] inputArr = null;
-            ArrayList<Integer> inputList = null;
+            System.out.println("\nCurrent cards on the field:\n" + board);
+            ArrayList<Integer> inputList = new ArrayList<Integer>();
             Scanner scan = new Scanner(System.in);
 
             while (board.anotherPlayIsPossible() && !board.isLegal(inputList)) {
+                int sizeOfArr = 0;
+                String[] inputArr = null;
+                inputList = new ArrayList<Integer>();
                 while (sizeOfArr != 2 && sizeOfArr != 3) {
-                    System.out.println("Please enter the indexes of the cards you wish to remove, separated by a space (Ex: 1 2)");
+                    System.out.print("Please enter the indexes of the cards you wish to remove, separated by a space (Ex:1 2): ");
                     inputArr = scan.nextLine().split(" ");
                     sizeOfArr = inputArr.length;
                 }
                 System.out.print("Chosen Cards: ");
                 for (String str : inputArr) {
-                    System.out.print(board.cardAt(Integer.getInteger(str)));
-                    inputList.add(Integer.getInteger(str));
+                    // System.out.println("DEBUGGIN'");
+                    System.out.print(board.cardAt(Integer.parseInt(str)) + " ");
+                    inputList.add(Integer.parseInt(str));
+                }
+                if (!board.isLegal(inputList)) {
+                    System.out.println("\nThe cards you've chosen do NOT add up to 11. Choose again ...");
                 }
             }
+            // after getting past the "isLegal()" gateway ...
+            System.out.println();
+            board.replaceSelectedCards(inputList);
+            System.out.println("There are " + board.deckSize() + " undealt cards remaining.");
+
+            // if there are no more legal plays, and you haven't won yet, that means you lose D:
+            if (!board.anotherPlayIsPossible() && !board.gameIsWon()) {
+                System.out.println("\n" + board);
+                System.out.println("No more plays are possible. GAME OVER.");
+                System.exit(0); // terminates the program without returning anything
+            }
         }
+        // if you get past all of those while loops, it means you have won!
+        System.out.println("You have beat the game! VICTORY! :D");
     }
 }
---
+++ src/Testing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Testing {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Hi there!");
+        String reply = scan.nextLine();
+        System.out.println(reply);
+    }
+}
